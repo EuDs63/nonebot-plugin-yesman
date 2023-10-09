89b6db7036d72fc3414f86ce8d5eafdd9257a820
EuDs63 <erica23187@gmail.com>
2023-10-09 22:02:00 +0800

    尝试修复:value is not a valid list

--- nonebot-plugin-yesman/config.py
+++ nonebot-plugin-yesman/config.py
@@ -6,5 +6,5 @@ class Config(BaseModel, extra=Extra.ignore):
     # 设置概率触发的阈值，例如0.5表示50%的概率
     yesman_threshold = 0.5
 
-    # 包含允许的user_id的数组
-    yesman_allowed_user_ids = [123456, 654321]
+    # 包含允许的user_id的数组，默认为空
+    yesman_allowed_user_ids = []
